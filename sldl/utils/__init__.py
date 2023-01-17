a07fde8fc666fffbe21aa6fade8273f627574e4a
Nikita Pavlichenko <pilot7747@gmail.com>
2023-01-17 19:17:16 +0000

    Remove old utils

--- sldl/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .models import get_checkpoint_path
-from .video import get_video_frames, frames_to_video, get_fps
-
-__all__ = [
-    'get_checkpoint_path',
-    'get_video_frames',
-    'frames_to_video',
-    'get_fps',
-]
